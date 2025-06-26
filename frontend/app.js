87c20ae0a3ef300408f48553145026ccfe4eac14
Sarvagra <sarvagras977@gmail.com>
2025-06-26 23:04:04 +0530

    added all animations

--- frontend/app.js
+++ frontend/app.js
@@ -1,6 +1,6 @@
 const nextButton = document.querySelector('.next-btn');
 const video = document.querySelector('.hero-video');
-const movieList = ['../res/hero-1.mp4', '../res/hero-2.mp4', '../res/hero-3.mp4', 'videos/hero-4.mp4'];
+const movieList = ['../res/hero-1.webm', '../res/hero-2.mp4'];
 let index = 0;
 nextButton.addEventListener('click', function() {
 	index += 1;
@@ -8,4 +8,52 @@ nextButton.addEventListener('click', function() {
 		index = 0;
 	}
 	video.src = movieList[index];
+});
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const homeBtn = document.getElementById("home-btn");
+    const eventsBtn = document.getElementById("events-btn");
+    const aboutBtn = document.getElementById("about-btn");
+    const contactBtn = document.getElementById("contact-btn");
+    
+    if (homeBtn) {
+        homeBtn.addEventListener("click", function(e) {
+            e.preventDefault();
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        });
+    }
+
+    if (eventsBtn) {
+        eventsBtn.addEventListener("click", function(e) {
+            e.preventDefault();
+            window.scrollTo({
+                top: 3250,
+                behavior: 'smooth'
+            });
+        });
+    }
+
+    if (aboutBtn) {
+        aboutBtn.addEventListener("click", function(e) {
+            e.preventDefault();
+            window.scrollTo({
+                top: 1000,
+                behavior: 'smooth'
+            });
+        });
+    }
+
+    if (contactBtn) {
+        contactBtn.addEventListener("click", function(e) {
+            e.preventDefault();
+            window.scrollTo({
+                top: 7250,
+                behavior: 'smooth'
+            });
+        });
+    }
 });
