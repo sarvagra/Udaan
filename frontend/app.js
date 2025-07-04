d697771f4dc5bf5ab9e434afbf4bd65f823e3711
Sarvagra <sarvagras977@gmail.com>
2025-07-04 21:43:38 +0530

    added better positioning

--- frontend/app.js
+++ frontend/app.js
@@ -1,6 +1,6 @@
 const nextButton = document.querySelector('.next-btn');
 const video = document.querySelector('.hero-video');
-const movieList = ['../res/hero-1.webm', '../res/hero-2.mp4'];
+const movieList = ['res/hero-1.webm', 'res/hero-2.mp4'];
 let index = 0;
 nextButton.addEventListener('click', function() {
 	index += 1;
